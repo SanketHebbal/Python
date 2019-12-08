5e2f0f8b8088b17d04e1dd4ee3f46dbe400f98e9
Sanket_Hebbal <40386449+SanketHebbal@users.noreply.github.com>
2019-12-08 11:48:22 +0530

    Update SendEmail.py

--- My_Codes/SendEmail.py
+++ My_Codes/SendEmail.py
@@ -6,7 +6,7 @@ mail = smtplib.SMTP('smtp.gmail.com',25)
 
 mail.ehlo()
 
-mail.login('sankethebbal100@gmail.com','sanketmcs18')
+mail.login('sankethebbal100@gmail.com','password')
 
 mail.sendmail('sankethebbal100gmail.com','sankethebbal100@gmail.com',content)
 
